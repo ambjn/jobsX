910981e6c0215cd682a541d8d40a58e75b659f37
Amber Jain <jainanuamber@gmail.com>
2023-05-16 09:50:49 +0530

    initialised basic structure

--- app/index.js
+++ app/index.js
@@ -1,12 +1,38 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { SafeAreaView, View } from "react-native";
+import React from "react";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
+import { ScrollView } from "react-native-gesture-handler";
 
 const Home = () => {
+  const router = useRouter();
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => <ScreenHeaderBtn />,
+          headerRight: () => <ScreenHeaderBtn />,
+          title: "",
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ flex: 1, padding: SIZES.medium }}>
+          <Welcome />
+          <Nearbyjobs />
+          <Popularjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 export default Home;
